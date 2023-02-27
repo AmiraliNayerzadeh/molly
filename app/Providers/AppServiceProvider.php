e8c827a28d959321ffb3c2f095b69222c0384d54
unknown <amiralinayerzadeh@gmail.com>
2023-02-27 09:21:50 +0330

    8.12.1401

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use mysql_xdevapi\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
