158277ccbfac2ea0951979c5cbc3e3b682d829a1
unknown <amiralinayerzadeh@gmail.com>
2023-04-28 12:46:35 +0430

    8.2.1402

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind('path.public' , function (){
            return base_path(). '/public_html/' ;
         });
+
+        app('view.finder')->addExtension('xml');
+
     }
 
 
